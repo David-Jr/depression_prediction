d0c4c3fa32aaed1e1f18fc65e9ac82d724994979
David Junior <58364586+David-Jr@users.noreply.github.com>
2024-10-14 01:12:55 +0200

    Integrated the prediction feature

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import pandas as pd
-
 import streamlit as st
+import pickle
+import xgboost as xgb
+
+
+def load_model(filename: str = "sxgradient_boost_model"):
+    try:
+        with open(f'models\{filename}.pkl', 'rb') as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        print(f"Model file '{filename}.pkl' not found.")
+
+
+def get_input_data():
+
+    pass
+
+
+def predict_depression(input_data: dict, model):
+
+    input_df = pd.DataFrame(input_data)
+    prediction = model.predict(input_df)
+
+    return prediction[0]
+
 
 if __name__ == '__main__':
     
@@ -97,9 +120,41 @@ if __name__ == '__main__':
         "How is your stress level?",
         options=stress_level_options,
         value="Neutral",
-    )
-    
-    # simple_input_data = {
+    )    
+
+    # input_data = {
+    #     'future_career_concerns': [2],
+    #     'study_load': [2],
+    #     'teacher_student_relationship': [5],
+    #     'sleep_quality': [4],
+    #     'academic_performance': [3],
+    #     'stress_level': [4],
+    # }
+    input_data = {
+        'future_career_concerns': [carrer_concerns_options.index(carreer_concerns)],
+        'study_load': [study_load_options.index(study_load)],
+        'teacher_student_relationship': [teacher_student_relationship_options.index(teacher_student_relationship)],
+        'sleep_quality': [sleep_quality_options.index(sleep_quality)],
+        'academic_performance': [academic_performance_options.index(academic_performance)],
+        'stress_level': [stress_level_options.index(stress_level)],
+    }
+
+
+    # st.write(input_data)
+
+    depression = predict_depression(input_data=input_data, model=load_model())
+    if depression <= 4:
+        st.write(f"{depression:.4} PHQ9 Scale - Minimal Depression")
+    if depression >= 5 and depression <= 9:
+        st.write(f"{depression:.4} PHQ9 Scale - Mild Depression")
+    if depression >= 10 and depression <= 14:
+        st.write(f"{depression:.4} PHQ9 Scale - Moderate Depression")
+    if depression >= 15 and depression <= 19:
+        st.write(f"{depression:.4} PHQ9 Scale - Moderately Severe Depression")
+    if depression >= 20 and depression <= 27:
+        st.write(f"{depression:.4} PHQ9 Scale - Severe Depression")
+
+    # input_data = {
     #     'future_career_concerns': [2],
     #     'study_load': [2],
     #     'teacher_student_relationship': [5],
